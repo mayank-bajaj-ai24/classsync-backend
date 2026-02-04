91040c6fbd37877740d9a7e337d079f3dda8cf9f
Mayank Bajaj <mayankbajaj.ai24@rvce.edu.in>
2026-02-05 01:27:35 +0530

    Update student.js

--- routes/student.js
+++ routes/student.js
@@ -87,7 +87,7 @@ router.post("/mark-attendance", async (req, res) => {
       lat,
       lng
     );
-    const MAX_DISTANCE = 50; // meters
+    const MAX_DISTANCE = 48; // meters
 
     if (d > MAX_DISTANCE) {
       return res
